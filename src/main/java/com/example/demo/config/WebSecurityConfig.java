d804a4f31f9ff68dd823ca16c5a360d50fc159dc
huangsy <huangsy@longshare.com>
2018-05-21 23:02:45 +0800

    初始化提交

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+
+/**
+ *
+ * @author WongCU
+ * @date 2018/5/21
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/private/**")
+                .hasRole("DOGS") //大小写识别
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .groupRoleAttribute("cn")
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
+    }
+}
---
+++ src/main/java/com/example/demo/controller/DemoAPIController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author WongCU
+ * @date 2018/5/21
+ */
+@RestController
+@RequestMapping("/api/private")
+public class DemoAPIController {
+
+    @GetMapping("/demo")
+    public String demo(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("Details:" + authentication.getDetails());
+        System.out.println("credentials:" + authentication.getCredentials());
+        System.out.println("principal:" + authentication.getPrincipal());
+        System.out.println("name:" + authentication.getName());
+        System.out.println("grantedAuthority:");
+        for(GrantedAuthority grantedAuthority : authentication.getAuthorities()){
+            System.out.println(grantedAuthority.getAuthority());
+        }
+        return "hello!";
+    }
+}
